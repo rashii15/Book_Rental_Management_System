7bbe03305502d6f9a6ede007e2d31040f5b0aa7c
rashii15 <rashindilavanya@gmail.com>
2025-11-11 15:23:45 +0530

    initial commit

---
+++ src/main/java/edu/RL/Starter.java
@@ -0,0 +1,22 @@
+package edu.RL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Starter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Dashboard.fxml"))));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/edu/RL/controller/DashboardFormController.java
@@ -0,0 +1,37 @@
+package edu.RL.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.TouchEvent;
+
+public class DashboardFormController {
+
+    @FXML
+    private JFXButton btnSignIn;
+
+    @FXML
+    private Label lblEmail;
+
+    @FXML
+    private Label lblForgotPwd;
+
+    @FXML
+    private Label lblPassword;
+
+    @FXML
+    private Label lblsignin;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    void forgotpasswordlink(TouchEvent event) {
+
+    }
+
+}
